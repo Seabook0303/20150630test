de26b89e12c9c5cad88818e02291204d5901df00
Seabook0303 <k.t.seabook0303@gmail.com>
2015-07-01 00:00:53 +0900

    change

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -2,4 +2,6 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 // Start off looking at Australia.
 viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/20150630test/cesium-starter-app-master/KML/test.kmz'));
+
+// Add on kml file
+viewer.dataSources.add(Cesium.KmlDataSource.load('/KML/test.kmz'));

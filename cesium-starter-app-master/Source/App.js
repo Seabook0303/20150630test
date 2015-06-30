4637b21324cb8bf05c666dcfe135305ffa23a695
Seabook0303 <k.t.seabook0303@gmail.com>
2015-06-30 23:03:01 +0900

    cesium

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -2,3 +2,9 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 // Start off looking at Australia.
 viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
+
+// Add an ArcGIS MapServer imagery layer
+var imageryLayers = viewer.imageryLayers;
+imageryLayers.addImageryProvider(new Cesium.ArcGisMapServerImageryProvider({
+    url : 'http://www.ga.gov.au/gis/rest/services/earth_science/GA_Surface_Geology_of_Australia_WM/MapServer'
+}));
